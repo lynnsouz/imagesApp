1869fb1c62d0fe8a56499f6b22110269f880ca14
Lynneker Souza <lynnsouz@gmail.com>
2024-06-15 19:03:56 -0300

    Image content view and adjusts

--- ImagesApp/ImageContentView.swift
+++ ImagesApp/ImageContentView.swift
@@ -5,4 +5,29 @@
 //  Created by Lynneker Souza on 15/06/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ImageContentView: View {
+    private let size: Double = 64
+    let url: String
+
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: url)) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: size, height: size)
+            } placeholder: {
+                ProgressView()
+                    .frame(width: size, height: size)
+            }
+            .background(.thickMaterial)
+            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 8, height: 8)))
+        }
+    }
+}
+
+#Preview {
+    ImageContentView(url: "")
+}
